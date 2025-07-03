70bc89c71817516f546263301f1dab0066013b97
Ester Locatelli <ester.locatelli@escola.pr.gov.br>
2025-07-03 14:40:56 -0300

    aula 9

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,26 @@
-criaCartao (
+criaCartao(
     'Anfíbios',
     'O que significa a palavra anfíbio?',
     '“Duas vidas” – porque vivem parte na água (girino) e parte na terra (adulto).'
 )
 
-criaCartao (
+criaCartao(
     'Anfíbios',
     'Como é a reprodução dos anfíbios?',
     'Fecundação externa, ovos na água, sem casca.'
 )
 
-criaCartao (
-    'Anfíbios',
+criaCartao(
+    'Anfibios',
     'Quais são os tipos de respiração dos anfíbios?',
     'Girino: branquial | Adulto: pulmonar e cutânea.'
+)
+
+criaCartao(
+    'Anfibios',
+'O que é metamorfose nos anfíbios?',
+'Transformação do girino aquático em adulto terrestre.'
+
+
+
 )
